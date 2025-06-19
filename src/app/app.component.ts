9a4420368657e472aff2818f70aef9bc688383b3
rafaelwnk <rafaelcarmo749@gmail.com>
2025-06-19 18:20:04 -0300

    feat: add history-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { HistoryPageComponent } from "./pages/history-page/history-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePageComponent],
-  template: '<app-home-page><app-home-page>'
+  imports: [RouterOutlet, HomePageComponent, HistoryPageComponent],
+  template: '<app-history-page><app-history-page>'
 })
 export class AppComponent {
 
