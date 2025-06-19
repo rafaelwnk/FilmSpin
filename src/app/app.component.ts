3d15fc6ac5610bdfb1d8e1de8707c46bf379c483
rafaelwnk <rafaelcarmo749@gmail.com>
2025-06-19 16:49:17 -0300

    feat: add home-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomePageComponent],
+  template: '<app-home-page><app-home-page>'
 })
 export class AppComponent {
-  title = 'FilmSpin';
+
 }
